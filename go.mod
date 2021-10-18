b79090dbb806cd5a9f70868edc068ba6930dc801
criptdestroyer <ahmademir1201@gmail.com>
2021-10-18 18:23:58 +0700

    init docker setup

---
+++ go.mod
@@ -0,0 +1,10 @@
+module github.com/aemiralfath/IH-Userland-Onboard
+
+go 1.17
+
+require (
+	github.com/go-chi/chi v1.5.4
+	github.com/golang-migrate/migrate v3.5.4+incompatible // indirect
+	github.com/jackc/pgx/v4 v4.13.0 // indirect
+	github.com/rs/zerolog v1.25.0 // indirect
+)
---
+++ main.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/go-chi/chi"
+	"github.com/go-chi/chi/middleware"
+)
+
+func main(){
+	r := chi.NewRouter()
+	r.Use(middleware.RequestID)
+	r.Use(middleware.Logger)
+	r.Use(middleware.Recoverer)
+
+	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("hello world\n"))
+	})
+
+	err := http.ListenAndServe(":8080", r)
+	if err != nil {
+		fmt.Println(err)
+	}
+}
