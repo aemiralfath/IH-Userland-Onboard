0366bb77d597a9d93a0e92a399a8e60625182bfa
aemiralfath <ahmademir1201@gmail.com>
2021-11-03 22:25:06 +0700

    http testing for auth endpoint done

--- api/handler/auth/reset_password_test.go
+++ api/handler/auth/reset_password_test.go
@@ -1,9 +1,12 @@
 package auth_test
 
 import (
+	"fmt"
 	"net/http"
 	"testing"
 
+	"github.com/aemiralfath/IH-Userland-Onboard/api/crypto"
+	mock_crypto "github.com/aemiralfath/IH-Userland-Onboard/api/crypto/mock"
 	"github.com/aemiralfath/IH-Userland-Onboard/api/handler/auth"
 	"github.com/aemiralfath/IH-Userland-Onboard/api/helper"
 	"github.com/aemiralfath/IH-Userland-Onboard/datastore"
@@ -25,7 +28,7 @@ func TestResetPassword(t *testing.T) {
 	headers.Add("content-type", "application/json")
 	headers.Add("X-Api-ClientID", "postman")
 
-	// w := &helper.ResponseWriter{}
+	w := &helper.ResponseWriter{}
 	r := &http.Request{
 		Header: headers,
 	}
@@ -43,4 +46,52 @@ func TestResetPassword(t *testing.T) {
 		Times(1).
 		Return(user.Email, nil)
 
+	userStore := mock_datastore.NewMockUserStore(ctrl)
+	userStore.EXPECT().
+		GetUserByEmail(gomock.Any(), gomock.Eq(user.Email)).
+		Times(1).
+		Return(user, nil)
+
+	lastThreePassword := []string{}
+	for i := 0; i < 2; i++ {
+		userPassword, _ := crypto.NewAppCrypto().HashPassword(fmt.Sprintf("-/P4s5w0Rd_!?-WhaT#v#r%d", i))
+		lastThreePassword = append(lastThreePassword, userPassword)
+	}
+
+	passwordStore := mock_datastore.NewMockPasswordStore(ctrl)
+	passwordStore.EXPECT().
+		GetLastThreePassword(gomock.Any(), gomock.Eq(user.ID)).
+		Times(1).
+		Return(lastThreePassword, nil)
+
+	mockCrypto := mock_crypto.NewMockCrypto(ctrl)
+	for _, e := range lastThreePassword {
+		mockCrypto.EXPECT().
+			ConfirmPassword(e, request.Password).
+			Return(false)
+	}
+
+	hashPassword, _ := crypto.NewAppCrypto().HashPassword(request.Password)
+	mockCrypto.EXPECT().
+		HashPassword(request.Password).
+		Times(1).
+		Return(hashPassword, nil)
+
+	userStore.EXPECT().
+		ChangePassword(gomock.Any(), user).
+		Times(1).
+		Return(nil)
+
+	passwordStore.EXPECT().
+		AddNewPassword(gomock.Any(), &datastore.Password{Password: hashPassword}, user.ID).
+		Times(1).
+		Return(nil)
+
+	handler := auth.ResetPassword(mockCrypto, userStore, passwordStore, otpStore)
+	handler(w, r)
+
+	result := w.GetBodyJSON()
+	if result["success"] != true {
+		t.Errorf("handler not success")
+	}
 }
